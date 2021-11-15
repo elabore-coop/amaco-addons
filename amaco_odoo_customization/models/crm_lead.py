cb794dda530e382db28481c3854c90b84be5ce9a
Stéphan Sainléger <contact@stephansainleger.fr>
2021-11-15 23:47:01 +0100

    [CLEAN] Remove useless method

--- amaco_odoo_customization/models/crm_lead.py
+++ amaco_odoo_customization/models/crm_lead.py
@@ -4,16 +4,9 @@ from odoo import models, api
 
 class Lead(models.Model):
     _inherit="crm.lead"
-    
-    @api.depends('active')
-    def archive_analytic_account(self):
-        import pdb; pdb.set_trace()
-        if self.analytic_account:
-            self.analytic_account.active = self.active
-    
+
     def write(self, vals):
         res = super(Lead, self).write(vals)
-        import pdb; pdb.set_trace()
         if self.analytic_account:
             self.analytic_account.active = self.active
         return res
