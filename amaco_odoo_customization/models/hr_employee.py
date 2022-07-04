e970d278dfe9e857c29a307255679a692ea38319
Stéphan Sainléger <contact@stephansainleger.fr>
2022-07-04 20:24:04 +0200

    [UPD] Complete list of departure_reasons

--- amaco_odoo_customization/models/hr_employee.py
+++ amaco_odoo_customization/models/hr_employee.py
@@ -7,8 +7,34 @@ class HrEmployeePrivate(models.Model):
     _inherit = "hr.employee"
 
     departure_reason = fields.Selection(
-        selection_add=[
-            ("contract_ended", "End of contract"),
-            ("agreed_termination", "Mutually agreed termination of contract"),
+        [
+            ("resigned", "Resigned"),
+            (
+                "agreed_termination",
+                "Mutually agreed termination of contract",
+            ),
+            ("redudancy_firing", "Redudancy Firing"),
+            (
+                "personnal_firing",
+                "Fired for personal reasons",
+            ),
+            ("retired", "Retired"),
+            ("fixed_term_contract_ended", "End of fixed-term contract"),
+            (
+                "fixed_term_contract_agreed_termination",
+                "Mutually agreed termination of fixed-term contract",
+            ),
+            (
+                "transition_fixed_term_to_permanent_contract",
+                "Transition from fixed-term to permanent contract",
+            ),
+            (
+                "fixed_term_contract_other_termination",
+                "End of fixed-term contract for other reasons",
+            ),
+            (
+                "try_period_termination",
+                "Try period termination",
+            ),
         ],
     )
