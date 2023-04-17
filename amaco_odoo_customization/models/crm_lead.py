89dcb4ea929ffada9e5b0e2f6b84d5f5213b312c
clementthomas <clement@cotyl.net>
2023-04-17 11:48:33 +0200

    [FIX] amaco_odoo_customization: error on lead creation

--- amaco_odoo_customization/models/crm_lead.py
+++ amaco_odoo_customization/models/crm_lead.py
@@ -20,7 +20,8 @@ class Lead(models.Model):
         sollicitation_project_id = self.env['ir.config_parameter'].sudo().get_param('sollicitation_project_id')
         # set new context without default_stage_id to avoid error on create
         ctx = self.env.context.copy()
-        ctx.pop('default_stage_id')
+        if 'default_stage_id' in ctx:
+            ctx.pop('default_stage_id')
         ctx['default_project_id'] = sollicitation_project_id
         project = self.env['project.task'].with_context(ctx).create({
             'project_id': int(sollicitation_project_id), 
