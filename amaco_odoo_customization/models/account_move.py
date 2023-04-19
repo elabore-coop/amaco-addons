ba6b032c65e768d370d4d7b36bdc131b55930625
clementthomas <clement@cotyl.net>
2023-04-19 09:34:32 +0200

    [IMP] amaco_odoo_customization: search supplier invoice from account analytic TASK: https://elabore.coop/web#view_type=form&model=project.task&id=1152&active_id=1152 + translation

--- amaco_odoo_customization/models/account_move.py
+++ amaco_odoo_customization/models/account_move.py
@@ -8,7 +8,9 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     invoice_origin = fields.Char(readonly=False)
-    analytic_account_id = fields.Many2one('account.analytic.account', "Analytic account", compute="compute_analytic_account_id")
+    analytic_account_id = fields.Many2one('account.analytic.account', "Analytic account", 
+                                          compute="compute_analytic_account_id", 
+                                          search="_search_analytic_account_id")
     
     @api.depends('invoice_line_ids')
     def compute_analytic_account_id(self):        
@@ -22,4 +24,24 @@ class AccountMove(models.Model):
                 move.analytic_account_id = lines_analytic_account_ids[0]
             else:
                 move.analytic_account_id = None
-            
+    
+    def _search_analytic_account_id(self, operator, value):
+        """
+            search all move with analytic account match id : 
+                - all move lines of the move having the same analytic account id
+            to do this, check if averrage equals max of analytic_account_id
+        """
+        analytic_accounts = self.env['account.analytic.account'].name_search(value,operator=operator)
+        if analytic_accounts:
+            self.env.cr.execute("""select am.id, max(aml.analytic_account_id) m
+                from account_move am left join account_move_line aml on aml.move_id = am.id 
+                group by am.id
+                having 
+                max(aml.analytic_account_id) = avg(aml.analytic_account_id)
+                and
+                max(aml.analytic_account_id) in %s
+                order by am.id
+                """, [tuple([a[0] for a in analytic_accounts])])
+            res = self.env.cr.fetchall()
+            return [('id','in',[t[0] for t in res])]
+        return [('id', 'in', [])]
