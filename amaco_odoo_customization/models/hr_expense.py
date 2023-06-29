b985cb7d3aa2b4b90d207f42f40de2f7704ec2d4
clementthomas <clement@cotyl.net>
2023-06-29 12:09:56 +0200

    [IMP] amaco_odoo_customization: in expenses, allow analytic account on multi company

--- amaco_odoo_customization/models/hr_expense.py
+++ amaco_odoo_customization/models/hr_expense.py
@@ -5,4 +5,5 @@ from odoo import fields, models
 class HrExpense(models.Model):
     _inherit = "hr.expense"
 
+    # add check_company = False to allow multi company selection
     analytic_account_id = fields.Many2one(required=True, check_company=False)
