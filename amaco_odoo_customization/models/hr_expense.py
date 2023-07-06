300b507421d55caf0cfa7c5f4e6be40e50018286
clementthomas <clement@cotyl.net>
2023-07-06 08:21:52 +0200

    [IMP] amaco_odoo_customization: hr expenses for multi company

--- amaco_odoo_customization/models/hr_expense.py
+++ amaco_odoo_customization/models/hr_expense.py
@@ -6,6 +6,15 @@ class HrExpense(models.Model):
     _inherit = "hr.expense"
 
     # add check_company = False to allow multi company selection
-    analytic_account_id = fields.Many2one(required=True, check_company=False)
-    employee_id = fields.Many2one(check_company=False)
+    #analytic_account_id = fields.Many2one(required=True, check_company=False)
+    employee_id = fields.Many2one(check_company=False)    
     project_id = fields.Many2one(check_company=False)
+
+
+class HrExpenseSheet(models.Model):
+    _inherit = "hr.expense.sheet"
+
+    # add check_company = False to allow multi company selection 
+    #analytic_account_id = fields.Many2one(check_company=False)
+    employee_id = fields.Many2one(check_company=False)
+    address_id = fields.Many2one(check_company=False)
