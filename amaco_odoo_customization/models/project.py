e715d54986e090d552c454ab971fc32b5c6ae819
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-03-01 10:52:43 +0100

    [IMP]amaco_odoo_customization:add expenses in projet overview

--- amaco_odoo_customization/models/project.py
+++ amaco_odoo_customization/models/project.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from odoo import _, models, fields
-
+from odoo.addons.sale_timesheet.models.project_overview import _to_action_data
+import json
 
 class Project(models.Model):
     _inherit = "project.project"
 
     team_id = fields.Many2one("crm.team", string=_("Pôle"))
+
+    def _plan_get_stat_button(self):
+        stat_buttons = super()._plan_get_stat_button()
+        if self.env.user.has_group('hr_expense.group_hr_expense_user'):
+            stat_buttons.append({
+                'name': _("Expenses"),
+                'count': self.env['hr.expense'].search_count([('project_id', 'in', self.ids)]),
+                'icon': 'fa fa-dollar',
+                'action': _to_action_data(
+                    'hr.expense',
+                    domain=[('project_id', 'in', self.ids)],
+                    context={'create': True, 'edit': True, 'delete': True}
+                ),
+            })
+        return stat_buttons
